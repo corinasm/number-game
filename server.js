2ca260fae6443fa1abf99db8a7092470bd3c837b
corinasm <corina.mitu@yahoo.com>
2018-07-19 22:40:18 -0700

    fixed routes so that it works properly after refresh

--- server.js
+++ server.js
@@ -11,35 +11,38 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(session({secret: 'alphacount'})); 
 
-app.get('/', function(request, response) {
-    console.log("request.session.ran_num = ", request.session.ran_num);
-    if ((typeof(request.session.ran_num) == "undefined")) {
-        request.session.ran_num = Math.floor((Math.random()*100)+1); 
-        console.log("Random Number: ", request.session.ran_num);
-        response.render('main', {message: '', showRes: "newGame" })
-    } else { 
-        var playerNum = request.session.player_num;
-        var ranNum = request.session.ran_num; 
-        if (playerNum > ranNum) {
-            response.render('main', {message: "Too High!", showRes: "high"}) 
-        } else if (playerNum < ranNum) {
-            response.render('main', {message: "Too Low!", showRes: "low"}) 
-        } else {
-            response.render('main', {message: "Good guess!", rightGuess: ranNum, messageGuess: "was the number.", showRes: "success"}) 
-        }   
-    }     
-})
+app.get('', function(request, response) {
+    console.log('/');
+    response.redirect('/new-game');
+});
+
+app.get('/new-game', function(request, response) {
+    console.log('/new-game');
+    request.session.ran_num = Math.floor((Math.random()*100)+1);
+    console.log("Random Number: ", request.session.ran_num);
+    response.render('main', {message: '', showRes: "newGame" });
+});
+
+app.get('/play', function(request, response) {
+    console.log('/play');
+    console.log("Player's Number: ", request.session.player_num);
+    const ranNum = request.session.ran_num;
+    const playerNum = request.session.player_num;
+    if (playerNum > ranNum) {
+        response.render('main', {message: "Too High!", showRes: "high"}) 
+    } else if (playerNum < ranNum) {
+        response.render('main', {message: "Too Low!", showRes: "low"}) 
+    } else {
+        response.render('main', {message: "Good guess!", rightGuess: ranNum, messageGuess: "was the number.", showRes: "success"}) 
+    }   
+});
+
 app.post('/process', function(request, response) {
     request.session.player_num = request.body.number;
     console.log("Player's Number: ", request.session.player_num);
-    response.redirect('/');
-})
-
-app.get('/new-game', function(request, response) {
-    request.session.destroy();
-    response.redirect('/');
-})
+    response.redirect('/play');
+});
 
 app.listen(8200, function() {
  console.log("listening on port 8200");
-})
+});
